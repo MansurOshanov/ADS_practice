cccd6ec7b99ab23c83cb9219a08399b46b8cb13b
Mans <mansur97.97@gmail.com>
2021-04-28 01:30:24 +0600

    Solved reverse integer by getting last digit and putting it in new integer

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,5 +1,6 @@
 class Solution:
-    def reverse(self, x: int) -> int:
+    def reverse_using_python(self, x: int) -> int:
+        
         sign = 1 if x >= 0 else -1
         x = x if x >= 0 else -x
         string_repr = str(x)
@@ -8,3 +9,25 @@ class Solution:
         if result >= -2**31 and result <= 2**31 - 1:
             return int(result)
         return 0
+    
+    def reverse(self, x: int) -> int:
+        
+        max_int = 2**31 - 1
+        min_int = -(2**31)
+        print(max_int)
+        print(min_int)
+        
+        sign = 1 if x>=0 else -1
+        x = x if x>=0 else -x
+        
+        result = 0
+        while x>0:
+            last_digit = x % 10
+            x = x // 10
+            result = result * 10 + last_digit
+            print(result)
+        result = result * sign
+        if result >= min_int and result <= max_int:
+            return int(result)
+        return 0
+        
