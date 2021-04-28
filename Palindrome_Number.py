aa78edae9dfa45d999d17a8f890a06d3abbdcd4d
Mans <mansur97.97@gmail.com>
2021-04-28 21:39:55 +0600

    Solved Palindrome number problem using O(1) space.

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,4 +1,16 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome_using_string_conversion(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
+    
+    def isPalindrome(self, x: int) -> bool:
+        
+        if x<0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        reverted_number = 0
+        while(x > reverted_number):
+            reverted_number = reverted_number*10 + (x%10)
+            x = x // 10
+            
+        return x == reverted_number or x == reverted_number // 10
         
