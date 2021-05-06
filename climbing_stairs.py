0125a114064238617fcfa2b57cf9cb38fce5dd64
Mans <mansur97.97@gmail.com>
2021-05-06 23:09:17 +0600

    Solved clibing stairs via DP (Recursion + memo) approach

---
+++ climbing_stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    global memo 
+    memo = dict()
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        if n not in memo:
+            memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return memo[n]
+    
