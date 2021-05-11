aa72dccffdb935614b24cbd279ef563bc5560bb7
Mans <mansur97.97@gmail.com>
2021-05-11 10:26:16 +0600

    Used BFS

---
+++ minimum_depth_of_binary_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        
+        queue = collections.deque()
+        depth = 1
+        queue.append(root)
+        
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+            
+                
+        
