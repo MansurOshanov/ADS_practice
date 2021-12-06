94d3a2f1cb71ff77619bd4ea15f9c68a69afc7eb
Mans <mansur97.97@gmail.com>
2021-12-06 18:01:05 +0600

    Added iterative solution in N^N time

--- letter_case_permutation.py
+++ letter_case_permutation.py
@@ -1,28 +1,49 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
+        #iteration: O(n^n) time
+#         output = [""]
         
+#         for char in s:
+#             temp = []
+#             if char.isalpha():
+#                 for o in output:
+#                     temp.append(o+char.lower())
+#                     temp.append(o+char.upper())
+#             else:
+#                 for o in output:
+#                     temp.append(o+char)
+#             output = temp
+        
+#         return output
+
+        '''
+        DFS appoach: Backtracking
+        '''
         output = []
-        cur_word = []
-        
-        def dfs(cur_word, i):
-            if i == len(s):
-                output.append("".join(cur_word))
-                return
+        def dfs(cur_string, i):
+            if len(cur_string) == len(s):
+                output.append("".join(cur_string))
+                return 
             
             if s[i].isalpha():
-                cur_word.append(s[i].lower())
-                dfs(cur_word, i+1)
-                cur_word.pop()
-                cur_word.append(s[i].upper())
-                dfs(cur_word, i+1)
-                cur_word.pop()
-            else:
-                cur_word.append(s[i])
-                dfs(cur_word, i+1)
-                cur_word.pop()
+                cur_string.append(s[i].lower())
+                dfs(cur_string, i+1)
+                cur_string.pop()
+                
+                cur_string.append(s[i].upper())
+                dfs(cur_string, i+1)
+                cur_string.pop()
             
-        dfs(cur_word, 0)
+            else:
+                cur_string.append(s[i])
+                dfs(cur_string, i+1)
+                cur_string.pop()
+            return 
+        
+        cur_string = []
+        dfs(cur_string, 0)
         return output
+        
                 
             
             
